8ccfc8bd395b551472a984521a5aa89496db9050
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-05-30 12:01:33 -0300

    feat: adiciona novas comidas

---
+++ atividade2.js
@@ -0,0 +1,10 @@
+let cardapio = ["pizza de calabresa", "lasanha",  "sushi", "salada Caesar", "pastel", "yakisoba", "feijoada" ];
+ 
+console.log("Antes do push");
+console.table(cardapio);
+
+// Adicionando novos produtos no cardápio
+cardapio.push( "hambúrguer artesanal", "wrap de frango");
+
+console.log("Depois do push");
+console.table(cardapio);
