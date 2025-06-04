2ca34108563313282cff7de870f097723883056c
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 15:08:03 -0300

    feat: Separa um relatorio da primeira semana

---
+++ atividade13.js
@@ -0,0 +1,4 @@
+let diasFaltas = [ "Carlos – Segunda", "Carlos – Terça","Carlos – Quarta","Carlos – Quinta", "Carlos – Sexta", "Carlos – Segunda (semana 2)","Carlos – Terça (semana 2)","Carlos – Quarta (semana 2)"];
+  
+console.log("Relatório primeira semana:", diasFaltas.slice(0, 5)); 
+
