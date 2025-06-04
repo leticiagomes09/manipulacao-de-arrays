2fa1a4ce0933296ce75575d65f3055721ce1fae3
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 16:11:28 -0300

    feat: Organiza vendas em ordem decrescente

---
+++ atividade24.js
@@ -0,0 +1,4 @@
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+
+let vendasOdenadasdecrescente = vendas.sort((a, b) => b - a);
+console.log(`Vendas em ordem decrescente: ${vendasOdenadasdecrescente}`);
