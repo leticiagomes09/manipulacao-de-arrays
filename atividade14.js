0ecd320c0f39ca5a7cbffb833e3813d933434c7f
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 15:12:38 -0300

    feat: separa os artigos mais recentes

---
+++ atividade14.js
@@ -0,0 +1,12 @@
+let artigosBlog = [
+    "Como usar Docker com Node.js",
+    "5 erros comuns em JavaScript",
+    "Introdução ao TypeScript",
+    "Clean Code na prática",
+    "Design Patterns úteis",
+    "Refatoração de código",
+    "Boas práticas de API REST",
+    "Segurança em aplicações web"
+  ];
+
+  console.log("Os 4 artigos mais recentes são:", artigosBlog.slice(0, 4)); 
