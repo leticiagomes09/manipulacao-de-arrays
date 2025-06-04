65d2d64f5a3ec03abfb27f7db8e3681b933ee5d3
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 16:15:26 -0300

    feat: Inverte sequencia do array

---
+++ atividade25.js
@@ -0,0 +1,15 @@
+let atualizacoes = [
+    "Backup diário concluído",
+    "Servidor reiniciado",
+    "Nova versão implantada",
+    "Logs exportados",
+    "Firewall atualizado",
+    "Monitoramento ativado",
+    "Banco de dados otimizado",
+    "Script de segurança executado"
+  ];
+
+  let atualizacoesInvertidas = atualizacoes.reverse();
+
+  console.log("Atualizações invertidas:" , atualizacoesInvertidas);
+  
