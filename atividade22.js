234dc04e6a4f81d4d690215389c9c42b7e398983
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 16:00:59 -0300

    feat: filtra médias maiores ou iguais a 7

---
+++ atividade22.js
@@ -0,0 +1,31 @@
+let alunos = [
+    { nome: "Carlos", media: 6.5 },
+    { nome: "Fernanda", media: 7.2 },
+    { nome: "Lucas", media: 8.1 },
+    { nome: "Marina", media: 5.9 },
+    { nome: "João", media: 7.0 },
+    { nome: "Aline", media: 9.3 }
+  ];
+
+  let alunosFiltrado = alunos.filter(aluno => aluno.media >= 7);
+
+  console.log(`Médias maiores ou iguais a 7: ${alunosFiltrado}`);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
