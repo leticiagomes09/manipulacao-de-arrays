1e9673a4d248dd366d8602f3e068fa81aa9d7f2f
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 15:41:05 -0300

    feat: Transforma lista em letras maiusculas

---
+++ atividade19.js
@@ -0,0 +1,14 @@
+let codigos = [
+    "abc123",
+    "def456",
+    "ghi789",
+    "jkl012",
+    "mno345",
+    "pqr678",
+    "stu901",
+    "vwx234"
+  ];
+
+  let codigosEmCaps = codigos.map(codigo => codigo.toUpperCase());
+
+  console.log("Códigos em maiúsculas:", codigosEmCaps); 
