6a9fa9ea1a5bbd520fab69ee2b09e0cb61ec0854
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 14:49:02 -0300

    feat: Busca na lista se a funcionaria compareceu

---
+++ atividade9.js
@@ -0,0 +1,3 @@
+let presenca = ["João Lima","Carla Nunes", "Amanda Souza","Bruno Ribeiro","Fernanda Sales"];
+  
+console.log(`A funcionária 'Amanda Souza' compareceu à reunião? ${presenca.includes("Amanda Souza")}`); 
