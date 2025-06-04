974902d8250536d9d3b344029092202df1370efc
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 14:52:55 -0300

    feat: Verifica um item no estoque

---
+++ atividade10.js
@@ -0,0 +1,3 @@
+let estoque = ["mesa para PC","cadeira gamer", "monitor 27 polegadas","hub USB","mouse sem fio"];
+  
+console.log(`No estoque tem 'cadeira gamer'? ${estoque.includes("cadeira gamer")}`);
