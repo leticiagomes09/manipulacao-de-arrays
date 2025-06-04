8b7dbc378c46dd3cc3dc12f21f14ecae810a7990
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-06-04 16:06:47 -0300

    feat: Organiza array em ordem alfabetica

---
+++ atividade23.js
@@ -0,0 +1,15 @@
+let candidatos = [
+    "Marcos",
+    "Aline",
+    "Carla",
+    "João",
+    "Fernanda",
+    "Lucas",
+    "Bruno",
+    "Isabela"
+  ];
+  
+  let candidatosOrdenados = candidatos.sort();
+  console.log("Candidatos em ordem alfabetica:");
+  console.table(candidatosOrdenados);
+  
